bec0fbcd29f7b3c7498af8df70e35b4896772c9e
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-09 10:04:09 -0300

    Revert "agregado la view para itemChange"
    This reverts commit bd9407fec03715ca1b367a916ef35b640736bcb8.

--- administracion/views.py
+++ administracion/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
 def itemList(request):
-	return render(request, 'list.html', {})
+	return render(request, 'list.html', {'items':Item.objects.all()})
+
+def itemChange(request, pk):
+    return render(request, 'itemChange.html', {'item': get_object_or_404(Item, pk=pk)})
