c94e94071564bbc69d8f3a366707a77aaaf4ec41
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-09 08:45:41 -0300

    primera prueba de la carta

--- administracion/views.py
+++ administracion/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Item
 
 def itemList(request):
-	return render(request, 'list.html', {})
+	return render(request, 'list.html', {'items':Item.objects.all()})
