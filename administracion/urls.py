bd9407fec03715ca1b367a916ef35b640736bcb8
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-09 09:57:44 -0300

    agregado la view para itemChange

--- administracion/urls.py
+++ administracion/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
 	path('', views.itemList, name='item_list'),
-	path('itemChange/<int:pk>/', views.itemChange, name='itemChange'),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Item
+from django.shortcuts import render
 
 def itemList(request):
-	return render(request, 'list.html', {'items':Item.objects.all()})
-
-def itemChange(request, pk):
-    return render(request, 'itemChange.html', {'item': get_object_or_404(Item, pk=pk)})
+	return render(request, 'list.html', {})
