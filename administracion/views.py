b9ad9f9edfef878133729bd42851cb11ad547f97
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-09 10:23:49 -0300

    se reestructura las templates

--- administracion/views.py
+++ administracion/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 def itemList(request):
-	return render(request, 'list.html', {'items':Item.objects.all()})
+	return render(request, 'admin/list.html', {'items':Item.objects.all()})
 
 def itemChange(request, pk):
-    return render(request, 'itemChange.html', {'item': get_object_or_404(Item, pk=pk)})
+    return render(request, 'admin/itemChange.html', {'item': get_object_or_404(Item, pk=pk)})
