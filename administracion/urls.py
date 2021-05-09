a476020b877f9e896e2cee2f77390b7d7480aa3e
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-09 12:34:42 -0300

    se agrega un menú para acceder a las acciones

--- administracion/urls.py
+++ administracion/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.itemList, name='itemList'),
+	path('', views.itemList, name='home'),
 	path('itemNew', views.itemNew, name='itemNew'),
 	path('itemChange/<int:pk>/', views.itemChange, name='itemChange'),
 ]
