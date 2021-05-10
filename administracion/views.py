d7e02cc5e4661c10f03854f9c9af31c5411404ce
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-10 13:27:45 -0300

    se agrega identificación al panel

--- administracion/views.py
+++ administracion/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Item
 from .forms import ItemForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
 	return render(request, 'admin/home.html', {'items':Item.objects.all()})
 
+@login_required
 def itemList(request):
 	return render(request, 'admin/items.html', {'items':Item.objects.all()})
 
+@login_required
 def itemInfo(request, pk):
 	return render(request, 'admin/itemInfo.html', {'item': get_object_or_404(Item, pk=pk)})
 
+@login_required
 def itemChange(request, pk):
 	item = get_object_or_404(Item, pk=pk)
 	form = ItemForm(request.POST, instance=item)
@@ -25,6 +30,7 @@ def itemChange(request, pk):
 	else:
 		return render(request, 'admin/itemChange.html', {'form': form})
 
+@login_required
 def itemNew(request):
 	if request.method == "POST":
 		form = ItemForm(request.POST)
