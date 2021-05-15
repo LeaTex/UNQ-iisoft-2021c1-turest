7b81a15478b0ffb8e8779fc4bf0b830f882988dd
Mailin Soñez <mailinsonez@gmail.com>
2021-05-15 14:32:19 -0300

    agrego urls para mozos y mozoNew

--- administracion/urls.py
+++ administracion/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
 	path('itemNew', views.itemNew, name='itemNew'),
 	path('itemInfo/<int:pk>/', views.itemInfo, name='itemInfo'),
 	path('itemChange/<int:pk>/', views.itemChange, name='itemChange'),
+	path('mozos', views.itemList, name='mozoList'),
+	path('mozoNew', views.itemNew, name='mozoNew'),
 ]
