c705144d1f224fbc5d570621f085da9ff3ac0da2
pablov1993 <oronopablo@hotmail.com.ar>
2021-05-24 16:18:54 -0300

    Agrego modelo de sector

--- administracion/models.py
+++ administracion/models.py
@@ -52,3 +52,16 @@ class Mesa(models.Model):
     def __str__(self):
         return self.mesa
 
+
+class Sector(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    sector = models.CharField(max_length=200)
+    mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.updateDate = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.sector
+
