21fa0efb3ef13f54febbc026da2f1ff96ee91ae9
Mariano Alvarez <mariano.e.alvarez@gmail.com>
2021-05-29 18:40:23 -0300

    Link a los items

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from administracion.models import Item
 
 
 def home(request):
     return render(request, 'portal/home.html', {'items': Item.objects.all()})
+
+
+#@login_required
+def itemView(request, pk):
+    return render(request, 'portal/itemView.html', {'item': get_object_or_404 (Item, pk=pk)})
