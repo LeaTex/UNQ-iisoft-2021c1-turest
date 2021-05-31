3d0fb743bfb5b88ab6dc55620fec28cab623c244
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-05-31 00:57:02 -0300

    agregado cierre de session para clientes

--- portal/views.py
+++ portal/views.py
@@ -3,17 +3,17 @@ from administracion.models import Item
 
 
 def home(request):
-    return render(request, 'portal/home.html', {'items': Item.objects.all()})
+    return render(request, 'portal/home.html', {'items': Item.objects.all(), 'pedidos':request.session.get('pedidos', [])})
 
 
 #@login_required
 def itemView(request, pk):
     if request.method == "POST":
         pedido = (request.POST['item'], request.POST['cantidad'])
-        if "pedidos" in request.session:
-            request.session["pedidos"].append(pedido)
+        if 'pedidos' in request.session:
+            request.session['pedidos'].append(pedido)
         else:
-            request.session["pedidos"] = [pedido]
+            request.session['pedidos'] = [pedido]
         return redirect('home')
     else:
         return render(request, 'portal/itemView.html', {'item': get_object_or_404(Item, pk=pk)})
