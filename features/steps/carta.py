7d3bf6c2eeef7c9934f51e5b75f479afd090bde4
LeaTex <leatex_99@yahoo.com>
2021-06-07 03:24:02 -0300

    menu cucumber feature

--- features/steps/carta.py
+++ features/steps/carta.py
@@ -1,23 +1,43 @@
-from behave import *
+from behave import given, when, then
+from administracion.models import Item
+from django.contrib.auth.models import User
 
 
-@given('a 50 $ orange juice')
-def step_impl(context):
-    pass
+@given(u'there are no items registered in the system')
+def items_list_is_empty(context):
+    assert Item.objects.all().count() == 0
 
-@given('a 400 $ full hamburguer')
-def step_impl(context):
-    pass
 
-@given('the menu with items in it')
-def step_impl(context):
-    pass
+@given(u'the customer enter the home page')
+def customer_enter_home_page(context):
+    context.response = context.test.client.get('/')
+    assert context.response.status_code == 200
 
-@when('a customer accesss the site')
-def step_impl(context):
-    assert True is not False
 
-@then('the menu with all items details is given')
-def step_impl(context):
-    assert context.failed is False
+@then(u'message "{message}" is displayed')
+def empty_list_message_displayed(context, message):
+    context.test.assertContains(context.response, message)
 
+
+@given(u'there is a "{item_price}" dollar "{item_name}" with "{item_description}" in the menu')
+def add_item_to_menu(context, item_price, item_name, item_description):
+    User.objects.create_user(username='test_user', email='user@test.com', password='TestUser12345')
+    Item.objects.create(author=User.objects.first(), name=item_name, description=item_description, price=float(item_price))
+    assert Item.objects.all().count() != 0
+
+@given(u'there are this three items')
+def add_three_items_to_menu(context):
+    User.objects.create_user(username='test_user', email='user@test.com', password='TestUser12345')
+    for row in context.table:
+        Item.objects.create(author=User.objects.first(), name=row['item_name'], description=row['item_description'], price=float(row['item_price']))
+    assert Item.objects.all().count() == 3
+
+
+@then(u'"{item_name}" is in menu')
+def step_impl(context, item_name):
+    context.test.assertContains(context.response, item_name)
+
+
+@then(u'message "{message}" is not displayed')
+def empty_list_message_displayed(context, message):
+    context.test.assertNotContains(context.response, message)
