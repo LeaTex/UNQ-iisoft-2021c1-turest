7ec2bacce0a22a163f2b1be32225869bbc72bbc0
Miguel Barraza <miguelbarraza2015@gmail.com>
2021-06-14 18:07:36 -0300

    agregado confirmar pedido

--- portal/urls.py
+++ portal/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login", LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout", LogoutView.as_view(template_name="registration/logout.html"), name="salir"),
-    path('itemView/<int:pk>/', views.itemView, name='itemView')
+    path('itemView/<int:pk>/', views.itemView, name='itemView'),
+    path('cartView/', views.cartView, name='cartView'),
+    path('cartConfirm/', views.cartConfirm, name='cartConfirm')
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from administracion.models import Item
 
 
 def home(request):
-    return render(request, 'portal/home.html', {'items': Item.objects.all(), 'pedidos': request.session.get('pedidos', [])})
+    tienePedidos = len(request.session['pedidos'])>0
+    return render(request, 'portal/home.html', {'items': Item.objects.all(), 'pedidos': tienePedidos})
 
 
 # @login_required
@@ -11,9 +13,25 @@ def itemView(request, pk):
     if request.method == "POST":
         pedido = (request.POST['item'], request.POST['cantidad'])
         if 'pedidos' in request.session:
-            request.session['pedidos'].append(pedido)
+            list = request.session['pedidos']
+            list.append(pedido)
+            request.session['pedidos'] = list
         else:
             request.session['pedidos'] = [pedido]
         return redirect('home')
     else:
         return render(request, 'portal/itemView.html', {'item': get_object_or_404(Item, pk=pk)})
+
+def cartView(request):
+    tienePedidos = len(request.session['pedidos'])>0
+    lista = []
+    for id,cantidad in request.session['pedidos']:
+        print("buscando", id)
+        item = Item.objects.get(pk=int(id))
+        lista.append((item, cantidad))
+    return render(request, 'portal/carrito.html', {'items': lista, 'pedidos': tienePedidos})
+
+@login_required
+def cartConfirm(request):
+    request.session['pedidos'] = []
+    return render(request, 'portal/confirmado.html', {})
