97076104c87d9e86cc10a3dba7d0165867d4237b
Mariano Alvarez <mariano.e.alvarez@gmail.com>
2021-06-14 18:42:01 -0300

    Aplicacion de buenas practicas

--- portal/views.py
+++ portal/views.py
@@ -5,7 +5,7 @@ from administracion.models import Item
 
 def home(request):
     if 'pedidos' in request.session:
-        tienePedidos = len(request.session['pedidos'])>0
+        tienePedidos = len(request.session['pedidos']) > 0
     else:
         tienePedidos = False
     return render(request, 'portal/home.html', {'items': Item.objects.all(), 'pedidos': tienePedidos})
@@ -25,14 +25,16 @@ def itemView(request, pk):
     else:
         return render(request, 'portal/itemView.html', {'item': get_object_or_404(Item, pk=pk)})
 
+
 def cartView(request):
-    tienePedidos = len(request.session['pedidos'])>0
+    tienePedidos = len(request.session['pedidos']) > 0
     lista = []
-    for id,cantidad in request.session['pedidos']:
+    for id, cantidad in request.session['pedidos']:
         print("buscando", id)
         item = Item.objects.get(pk=int(id))
         lista.append((item, cantidad))
     return render(request, 'portal/carrito.html', {'items': lista, 'pedidos': tienePedidos})
+
 
 @login_required
 def cartConfirm(request):
