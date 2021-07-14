c7ad4325c71d8491152eac2204a7994b18c3e6fb
LeaTex <leatex_99@yahoo.com>
2021-07-13 23:55:30 -0300

    update order. first scenario

--- features/pedido.feature
+++ features/pedido.feature
@@ -2,14 +2,13 @@ Feature: Eliminar un item del pedido
   Como cliente quiero poder eliminar un item de un pedido para no tenerlo mas en la solicitud y que no me lo entreguen
 
   Scenario: customer deletes one item from the order with two items 
-      Given an order with 1 milanesa and 2 helado
-        When the customer clicks on eliminar button next to milanesa item 
-       Then the order shows only 2 helado 
-       But the order does not show 1 milanesa
+      Given a menu with a "150" pesos "milanesa" and "85" pesos "helado"
+        And an order with "1" "milanesa" and "2" "helado" of "320" pesos total cost
+       When the customer clicks on "Eliminar" button next to "milanesa" item
+       Then the order shows only "2" "helado" of "170" pesos total cost
+        But the order does not show any "milanesa"
 
-  Scenario: customer deletes one item from the order with only one item  
-      Given an order with 1 milanesa
-        When the customer clicks on eliminar button next to milanesa item 
-       Then the message "No hay productos agregados en tu pedido." is displayed
-
-    
+  # Scenario: customer deletes one item from the order with only one item  
+  #     Given an order with 1 milanesa
+  #      When the customer clicks on eliminar button next to milanesa item 
+  #      Then the message "No hay productos agregados en tu pedido." is displayed
--- features/steps/pedido.py
+++ features/steps/pedido.py
@@ -2,32 +2,51 @@ from behave import given, when, then
 from administracion.models import Item
 from django.contrib.auth.models import User
 
-@given(u'an order with 1 milanesa and 2 helado')
-def step_impl(context):
-    context.response = context.test.client.get('cartView/')
-    assert context.response.status_code == 200
+@given(u'a menu with a "{mila_price}" pesos "{mila_item_name}" and "{helado_price}" pesos "{helado_item_name}"')
+def menu_with_two_items(context, mila_item_name, mila_price, helado_item_name, helado_price):
+    User.objects.create_user(username='test_user', email='user@test.com', password='TestUser12345')
+    Item.objects.create(author=User.objects.first(), name=mila_item_name, description=mila_item_name, price=int(mila_price))
+    Item.objects.create(author=User.objects.first(), name=helado_item_name, description=helado_item_name, price=int(helado_price))
+    assert Item.objects.all().count() == 2
 
 
-@when(u'the customer clicks on eliminar button next to milanesa item')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When the customer clicks on eliminar button next to milanesa item')
+@given(u'an order with "{mila_amount}" "{mila_item_name}" and "{helado_amount}" "{helado_item_name}" of "{total_cost}" pesos total cost')
+def order_with_two_items(context, mila_amount, mila_item_name, helado_amount, helado_item_name, total_cost):
+
+    item = Item.objects.filter(name=mila_item_name).first()
+    context.test.client.post(f'/itemView/{item.id}/', {'cantidad': int(mila_amount), 'item': item.id})
+
+    item = Item.objects.filter(name=helado_item_name).first()
+    context.test.client.post(f'/itemView/{item.id}/', {'cantidad': int(helado_amount), 'item': item.id})
+
+    context.response = context.test.client.get('/cartView/')
+    print(str(context.response.content))
+    context.test.assertContains(context.response, f'({mila_amount}) {mila_item_name}')
+    context.test.assertContains(context.response, f'({helado_amount}) {helado_item_name}')
+    context.test.assertContains(context.response, f'{total_cost}')
 
 
-@then(u'the order shows only 2 helado')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the order shows only 2 helado')
+@when(u'the customer clicks on "{delete_item_btn}" button next to "{mila_item_name}" item')
+def step_impl(context, delete_item_btn, mila_item_name):
+    # opción 1:
+    # buscar el link/botón y hacerle clic
+    # complicada sin selenium
+
+    # opción 2:
+    # emular el click utilizando el request
+    # el primer elemento del pedido tiene id 0
+    context.response = context.test.client.get('/cartItemDelete/0/')
+    assert context.response.status_code == 302 # redirect
 
 
-@then(u'the order does not show 1 milanesa')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the order does not show 1 milanesa')
+@then(u'the order shows only "{helado_amount}" "{helado_item_name}" of "{total_cost}" pesos total cost')
+def updated_order_with_one_item(context, helado_amount, helado_item_name, total_cost):
+    context.response = context.test.client.get('/cartView/')
+
+    context.test.assertContains(context.response, f'({helado_amount}) {helado_item_name}')
+    context.test.assertContains(context.response, f'{total_cost}')
 
 
-@given(u'an order with 1 milanesa')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given an order with 1 milanesa')
-
-
-@then(u'the message "{message}" is displayed')
-def step_impl(context,message):
-    context.test.assertContains(context.response, message)
+@then(u'the order does not show any "{mila_item_name}"')
+def updated_order_without_deleted_item(context, mila_item_name):
+    context.test.assertNotContains(context.response, mila_item_name)
